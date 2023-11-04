0c3c0d5c2f900784e13bf5c21b6ff49705715302
mervat <mervat.ramtaha@hotmail.com>
2023-11-04 18:37:25 +0200

    update gradle safeargs 0     id("androidx.navigation.safeargs") version "2.5.0" apply false

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,9 @@
 plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0-RC" apply false
-
+    id("androidx.navigation.safeargs") version "2.5.0" apply false
 }
-buildscript {
-    repositories {
-        google()
-    }
-    dependencies {
-        //noinspection GradlePluginVersion
-
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.5")
-    }
 
 
-}
+
+
